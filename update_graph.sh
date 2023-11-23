3e697984cdb67916034a48106cccb08ca23a1f65
Daniel_Stoxreiter <daniel.stoxreiter@outlook.de>
2023-11-23 16:44:38 +0100

    added workflow for uploading graphs

---
+++ update_graph.sh
@@ -0,0 +1,20 @@
+# bin/bash
+
+echo "delete namedgraphs"
+curl -D- -X DELETE \
+     "${R_ENDPOINT_V}?c=<https://sk.acdh.oeaw.ac.at/general>&c=<https://sk.acdh.oeaw.ac.at/model>&c=<https://sk.acdh.oeaw.ac.at/provenance>"
+sleep 300
+
+echo "add namedgraph sk_model.trig"
+curl $R_ENDPOINT_V \
+    -H 'Content-Type: application/x-trig; charset=UTF-8' \
+    -H 'Accept: text/boolean' \
+    -d @rdf/sk_model.trig
+sleep 600
+
+echo "add namedgraph general.trig"
+curl $R_ENDPOINT_V \
+    -H 'Content-Type: application/x-trig; charset=UTF-8' \
+    -H 'Accept: text/boolean' \
+    -d @rdf/general.trig
+sleep 600
