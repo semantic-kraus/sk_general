029a4b1a933ccf4c6b6754fbb84fbd752daa2fde
Daniel Elsner <74879332+linxOD@users.noreply.github.com>
2023-11-24 09:09:13 +0100

    Update update_graph.sh

--- update_graph.sh
+++ update_graph.sh
@@ -9,18 +9,18 @@ echo "add namedgraph sk_model.trig"
 curl $R_ENDPOINT_V \
     -H 'Content-Type: application/x-trig; charset=UTF-8' \
     -H 'Accept: text/boolean' \
-    -d @rdf/sk_model.trig
+    -d @sk_model.trig
 sleep 600
 
 echo "add namedgraph general.trig"
 curl $R_ENDPOINT_V \
     -H 'Content-Type: application/x-trig; charset=UTF-8' \
     -H 'Accept: text/boolean' \
-    -d @rdf/general.trig
+    -d @general.trig
 sleep 600
 
 echo "add namedgraph provenance.trig"
 curl $R_ENDPOINT_V \
     -H 'Content-Type: application/x-trig; charset=UTF-8' \
     -H 'Accept: text/boolean' \
-    -d @rdf/provenance.trig
+    -d @provenance.trig
